fdc922570ba33da274c4b66fc26e8410ecbc81fc
crocodiling <swings.chalky_0l@icloud.com>
2025-05-21 13:03:05 +0900

    Add files via upload

---
+++ src/basiccodes/ForSample.java
@@ -0,0 +1,8 @@
+package basiccodes;
+public class ForSample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
